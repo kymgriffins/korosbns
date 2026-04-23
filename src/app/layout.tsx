9690e7015a227defd59d810bb94eab273e2b8c66
kymgriffins <kimutaigriffins1960@gmail.com>
2026-04-23 20:49:30 +0300

    refactor: reorder imports and improve code formatting in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
-import { base, heading, handwriting } from "@/constants";
-import "@/styles/globals.css";
-import { cn, generateMetadata } from "@/utils";
-import Providers from "@/components/global/providers";
 import FlareCursor from "@/components/global/flare-cursor";
 import LoadingScreen from "@/components/global/loading-screen";
+import Providers from "@/components/global/providers";
+import { base, handwriting, heading } from "@/constants";
+import "@/styles/globals.css";
+import { cn, generateMetadata } from "@/utils";
 import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = generateMetadata();
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body
-                className={cn(
-                    "min-h-dvh bg-background text-foreground font-base antialiased overflow-x-hidden dark",
-                    base.variable,
-                    heading.variable,
-                    handwriting.variable,
-                )}
-            >
-                <Providers>
-                    <LoadingScreen />
-                    <FlareCursor />
-                    {children}
-                </Providers>
-                <Analytics />
-            </body>
-        </html>
-    );
-};
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-dvh bg-background text-foreground font-base antialiased overflow-x-hidden dark",
+          base.variable,
+          heading.variable,
+          handwriting.variable,
+        )}
+      >
+        <Providers>
+          <LoadingScreen />
+          <FlareCursor />
+          {children}
+        </Providers>
+        <Analytics />
+      </body>
+    </html>
+  );
+}
