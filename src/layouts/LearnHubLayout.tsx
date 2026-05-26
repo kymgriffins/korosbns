be6ea8117aeb8fa368622a8eb04cf79c142b13b4
kymgriffins <kimutaigriffins1960@gmail.com>
2026-05-26 14:15:32 +0300

    Fix mobile layout: make main container full height and add bottom padding for sticky navbar

--- src/layouts/LearnHubLayout.tsx
+++ src/layouts/LearnHubLayout.tsx
@@ -269,7 +269,7 @@ function LearnAppShell({ children }: { children: React.ReactNode }) {
       )}
 
       {/* 🚀 Main Content Canvas */}
-      <main className="flex-1 min-h-[calc(100dvh-56px)] md:min-h-screen pb-[72px] md:pb-0 flex flex-col">
+      <main className="flex-1 min-h-screen pb-16 md:pb-0 flex flex-col">
         {children}
       </main>
 
