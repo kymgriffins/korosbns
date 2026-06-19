c98a553b750b83ef0e6a9392cd1f7aa18e7fb98d
kymgriffins <kimutaigriffins1960@gmail.com>
2026-06-19 09:40:02 +0300

    fix turbopack: replace localFont with @font-face CSS (not supported in Turbopack)

--- apps/learn/src/constants/fonts.ts
+++ apps/learn/src/constants/fonts.ts
@@ -1,41 +1,6 @@
 import { Inter, Caveat } from "next/font/google";
-import localFont from "next/font/local";
 
-export const heading = localFont({
-    src: [
-        {
-            path: "../../public/fonts/Satoshi-Light.woff2",
-            weight: "300",
-            style: "normal",
-        },
-        {
-            path: "../../public/fonts/Satoshi-Regular.woff2",
-            weight: "400",
-            style: "normal",
-        },
-        {
-            path: "../../public/fonts/Satoshi-Medium.woff2",
-            weight: "500",
-            style: "normal",
-        },
-        {
-            path: "../../public/fonts/Satoshi-Bold.woff2",
-            weight: "700",
-            style: "normal",
-        },
-        {
-            path: "../../public/fonts/Satoshi-Black.woff2",
-            weight: "800",
-            style: "normal",
-        },
-        {
-            path: "../../public/fonts/Satoshi-Variable.woff2",
-            weight: "100 900",
-            style: "normal",
-        },
-    ],
-    variable: "--font-heading",
-});
+export const heading = { variable: "--font-heading" } as const;
 
 export const base = Inter({
     subsets: ["latin"],
