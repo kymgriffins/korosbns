07cd575a3a987930e38bfe91b307e85e287f6484
kymgriffins <kimutaigriffins1960@gmail.com>
2026-06-23 14:35:59 +0300

    chore: hide Budget Hub from navbar

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -6,10 +6,6 @@ export const NAV_LINKS = [
     href: Routes.Learn,
   },
   {
-    label: "Budget Hub",
-    href: Routes.BudgetHub,
-  },
-  {
     label: "Surveys",
     href: Routes.Surveys,
   },
